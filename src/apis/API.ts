0bb8ca96b16da39f552b2ac98265b78f0d16ad60
박현우 <gusdn3477@naver.com>
2025-02-19 17:13:07 +0900

    feat: axios를 래핑한 API 객체 구현

---
+++ src/apis/API.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// Axios 인스턴스 생성
+const API = axios.create({
+  baseURL: import.meta.env.BASE_URL, // 기본 URL
+  headers: {
+    'Content-Type': 'application/json', // 기본 헤더 설정
+  },
+});
+
+export default API;
