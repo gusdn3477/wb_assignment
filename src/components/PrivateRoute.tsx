bf44bf6318750c6fa213c9ed6ea9b3d780b05c57
박현우 <gusdn3477@naver.com>
2025-02-20 11:11:33 +0900

    fix: location -> window.location 아닌 useLocation()에서 사용하도록 수정

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,8 +1,9 @@
-import { Navigate, Outlet, useParams } from 'react-router-dom';
+import { Navigate, Outlet, useLocation, useParams } from 'react-router-dom';
 import { NAV_PATH, ROLE } from '@/constants';
 
 export default function PrivateRoute() {
   const { role } = useParams();
+  const location = useLocation();
   const currentPage = location.pathname.split('/')[2];
 
   if (role !== ROLE.ADMIN && currentPage === NAV_PATH.USERS) {
