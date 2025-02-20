e2b560640ade5cd61489eb32a82417ecd6710bad
박현우 <gusdn3477@naver.com>
2025-02-20 11:10:51 +0900

    feat: 어드민 아닌 경우 사용자 리스트 페이지 진입 시 캠페인 리스트로 Navigate 시키도록 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
+import PrivateRoute from './components/PrivateRoute';
 import CampaignManagament from './pages/CampaignManagament';
 import UserManagement from './pages/UserManagement';
 
@@ -9,7 +10,7 @@ export default function Router() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Navigate to="/admin/campaigns" />} />
-          <Route path="/:role">
+          <Route path="/:role" element={<PrivateRoute />}>
             <Route path="campaigns" element={<CampaignManagament />} />
             <Route path="users" element={<UserManagement />} />
           </Route>
---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet, useParams } from 'react-router-dom';
+import { NAV_PATH, ROLE } from '@/constants';
+
+export default function PrivateRoute() {
+  const { role } = useParams();
+  const currentPage = location.pathname.split('/')[2];
+
+  if (role !== ROLE.ADMIN && currentPage === NAV_PATH.USERS) {
+    return <Navigate to={`/${role}/${NAV_PATH.COMPAIGNS}`} />;
+  }
+  return <Outlet />;
+}
