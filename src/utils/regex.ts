e6039031bbbb1d10d0df6f853fe4f3fcc77da5ba
박현우 <gusdn3477@naver.com>
2025-02-24 17:19:58 +0900

    feat: 정규표현식 구현

---
+++ src/utils/regex.ts
@@ -0,0 +1,3 @@
+export const emailRegex = /^(?!^.{9,50}$|^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$).*$/;
+export const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,15}$/;
+export const nameRegex = /^[가-힣A-Za-z]{1,16}$/;
