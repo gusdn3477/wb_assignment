ad341565df2782c64948605bf8c76e2fac304518
박현우 <gusdn3477@naver.com>
2025-02-24 14:32:44 +0900

    feat: Grid 하단에 배치되는 Pagination 컴포넌트 고도화

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,9 +4,15 @@ interface PaginationProps {
   currentPage: number;
   totalPages: number;
   onPageChange: (page: number) => void;
+  className?: string;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+  className,
+}) => {
   const handlePageClick = (page: number) => {
     if (page >= 1 && page <= totalPages) {
       onPageChange(page);
@@ -14,36 +20,38 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
   };
 
   return (
-    <div className="flex h-32 items-center space-x-2">
-      <button onClick={() => handlePageClick(1)} disabled={currentPage === 1}>
-        ≪
-      </button>
-      <button onClick={() => handlePageClick(currentPage - 1)} disabled={currentPage === 1}>
-        〈
-      </button>
-      {[...Array(totalPages)].map((_, i) => {
-        const page = i + 1;
-        return (
-          <button
-            key={page}
-            onClick={() => handlePageClick(page)}
-            className={
-              page === currentPage ? 'bg-blue-500 px-2 py-1 font-bold text-white' : 'px-2 py-1'
-            }
-          >
-            {page}
-          </button>
-        );
-      })}
-      <button
-        onClick={() => handlePageClick(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        〉
-      </button>
-      <button onClick={() => handlePageClick(totalPages)} disabled={currentPage === totalPages}>
-        ≫
-      </button>
+    <div className={className}>
+      <div className="flex h-32 items-center space-x-2">
+        <button onClick={() => handlePageClick(1)} disabled={currentPage === 1}>
+          ≪
+        </button>
+        <button onClick={() => handlePageClick(currentPage - 1)} disabled={currentPage === 1}>
+          〈
+        </button>
+        {[...Array(totalPages)].map((_, i) => {
+          const page = i + 1;
+          return (
+            <button
+              key={page}
+              onClick={() => handlePageClick(page)}
+              className={
+                page === currentPage ? 'bg-blue-500 px-2 py-1 font-bold text-white' : 'px-2 py-1'
+              }
+            >
+              {page}
+            </button>
+          );
+        })}
+        <button
+          onClick={() => handlePageClick(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          〉
+        </button>
+        <button onClick={() => handlePageClick(totalPages)} disabled={currentPage === totalPages}>
+          ≫
+        </button>
+      </div>
     </div>
   );
 };
