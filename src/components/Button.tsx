15f80d82838491b650c7c9c1625e789504d5d09a
박현우 <gusdn3477@naver.com>
2025-02-24 17:55:23 +0900

    chore: Button disabled props 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,8 +3,15 @@ interface ButtonProps {
   variant?: 'primary' | 'secondary';
   className?: string;
   onClick: () => void;
+  disabled?: boolean;
 }
-export default function Button({ text, variant = 'primary', className, onClick }: ButtonProps) {
+export default function Button({
+  text,
+  variant = 'primary',
+  className,
+  onClick,
+  disabled = false,
+}: ButtonProps) {
   const getClassName = () => {
     if (variant === 'primary') {
       return 'bg-blue-500 text-white';
@@ -16,8 +23,9 @@ export default function Button({ text, variant = 'primary', className, onClick }
   return (
     <button
       type="button"
-      className={`${getClassName()} h-10 w-16 rounded-md ${className}`}
+      className={`${getClassName()} h-10 w-16 rounded-md ${className} ${disabled ? 'opacity-0.7 cursor-not-allowed' : ''}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
