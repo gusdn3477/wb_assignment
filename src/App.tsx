20b4664f644fc8250cfff06e83600ccab277a269
박현우 <gusdn3477@naver.com>
2025-02-24 16:27:09 +0900

    feat: 에러 모달 상단에 배치(API 요청 실패 시 상태만 바꿔주면 전역적으로 뜨게끔)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Router from './Router';
+import ErrorModal from './components/ErrorModal';
 import AuthProvider from './provider/AuthProvider';
+import { useModalStore } from './store/useModalStore';
 
 export default function App() {
+  const modalVisible = useModalStore((state) => state.visible.error);
+  const handleModalClose = useModalStore((state) => state.handleClose);
+
   return (
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
+      <ErrorModal
+        open={modalVisible}
+        onClose={() => {
+          handleModalClose('error');
+        }}
+      />
+    </>
   );
 }
---
+++ src/store/useModalStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+
+type ModalType = keyof ModalState['visible'];
+
+interface ModalState {
+  visible: {
+    error: boolean;
+  };
+}
+
+interface ModalAction {
+  handleOpen: (type: ModalType) => void;
+  handleClose: (type: ModalType) => void;
+}
+
+export const useModalStore = create<ModalState & ModalAction>((set) => ({
+  // 초기 상태
+  visible: {
+    error: false,
+  },
+  handleOpen: (type) => set((state) => ({ visible: { ...state.visible, [type]: true } })),
+  handleClose: (type) => set((state) => ({ visible: { ...state.visible, [type]: false } })),
+}));
